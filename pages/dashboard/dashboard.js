97b3370fa5d6a01b5ab1f6b5b4c0c5d177e50b8e
sthembizo.molefi <sthembizo@psybergate.co.za>
2024-07-31 12:19:35 +0200

    dashboard

---
+++ pages/dashboard/dashboard.js
@@ -0,0 +1,21 @@
+document.getElementById("taxInfoForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const form = document.getElementById("taxInfoForm");
+  const formData = new FormData(form);
+  const taxInfo = new TaxInfo();
+
+  taxInfo.setSalary(formData.get("salary"));
+  taxInfo.setBonuses(formData.get("bonuses"));
+  taxInfo.setInterestReceived(formData.get("interestReceived"));
+  taxInfo.setDividends(formData.get("dividends"));
+  taxInfo.setTotalCapitalGain(formData.get("totalCapitalGain"));
+  taxInfo.setRetirementFunds(formData.get("retirementFunding"));
+  taxInfo.setTravelAllowance(formData.get("travelAllowance"));
+
+    if(localStorage.getItem("taxInfoList")==null){
+        localStorage.setItem("taxInfoList","[]")
+
+    }
+  console.log(localStorage.getItem("taxInfoList"));
+});
