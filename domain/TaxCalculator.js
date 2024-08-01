40cadffbd15fdd3425301092f613dae15e17c50c
sthembizo.molefi <sthembizo@psybergate.co.za>
2024-08-01 13:58:16 +0200

    tax calculator object

--- domain/TaxCalculator.js
+++ domain/TaxCalculator.js
@@ -1,7 +1,17 @@
 class TaxCalculator{
 
     #taxInfo;
-    #TotalTaxableIncome;
+    #totalTaxableIncome;
+    #taxableInterest;
+    #taxableDividends;
+    #taxableCapitalGain;
+    #totalExpense;
+    #amountToBeTaxed;
+    #taxPayable;
+    #medicalCredit;
+    #finalTaxPayable;
+    #deductibleTravel;
+    #deductibleRetirementFunds;
 
     constructor(taxInfo){
         if(taxInfo instanceof TaxInfo){
@@ -10,8 +20,81 @@ class TaxCalculator{
     }
 
     compute(){
-        this.#TotalTaxableIncome=this.#taxInfo.getSalary()+this.#taxInfo.getBonuses();
+
+        this.calculateTotalTaxableIncome();
+        this.calculateTotalExpense();
+        this.#amountToBeTaxed=this.#totalTaxableIncome-this.#totalExpense;
+        console.log(this.#totalTaxableIncome);
+        console.log(this.#totalExpense);
+        console.log(this.#amountToBeTaxed)
+        this.calculateTaxPayable(this.#amountToBeTaxed);
+        console.log(this.#taxPayable)
         
+        this.calculateFinalTaxPayable();
+        // console.log(this.#finalTaxPayable);
+        
+
+
+    }
+
+    calculateTotalExpense(){
+        this.calculateDeductibleTravel();
+        this.calculateDeductibleRetirementFunding();
+        this.#totalExpense=this.#deductibleRetirementFunds+this.#deductibleTravel;
+        
+    }
+
+    calculateDeductibleTravel(){
+        if(Number(this.#taxInfo.getTravelAllowance()) <= TaxYear2023.TRAVEL_EXEMPTION){
+            this.#deductibleTravel=Number(this.#taxInfo.getTravelAllowance());
+        }else{
+            this.#deductibleTravel=TaxYear2023.TRAVEL_EXEMPTION;
+        }
+    }
+
+    calculateDeductibleRetirementFunding(){
+        if(Number(this.#taxInfo.getRetirementFunds()) <= TaxYear2023.RETIREMENT_EXEMPTION){
+            this.#deductibleRetirementFunds=Number(this.#taxInfo.getRetirementFunds());
+        }else{
+            this.#deductibleRetirementFunds=TaxYear2023.RETIREMENT_EXEMPTION;
+        }
+    }
+
+    calculateTaxPayable(AmountToBeTaxed){
+        this.#taxPayable=TaxYear2023.calculateTaxPayable(AmountToBeTaxed);
+    }
+
+    calculateMedicalCredits(dependents){
+        this.#medicalCredit=TaxYear2023.calculateMedicalCredits(dependents);
+    }
+
+    calculateFinalTaxPayable(){
+        this.calculateMedicalCredits(Number(this.#taxInfo.getDependents()));
+        this.#finalTaxPayable=this.#taxPayable-this.#medicalCredit-TaxYear2023.PRIMARY_REBATE-Number(this.#taxInfo.getTaxPaid());
+    }
+
+    calculateTotalTaxableIncome(){
+        this.calculateTaxableInterest();
+        this.#taxableDividends=0;
+        this.calculateTaxableCapitalGain();
+        this.#totalTaxableIncome=Number(this.#taxInfo.getSalary())+Number(this.#taxInfo.getBonuses())+this.#taxableInterest+this.#taxableDividends+this.#taxableCapitalGain;
+
+    }
+
+    calculateTaxableInterest(){
+        if(Number(this.#taxInfo.getInterestReceived()) <= TaxYear2023.INTEREST_EXEMPTION){
+            this.#taxableInterest=0;
+        }else{
+            this.#taxableInterest=Number(this.#taxInfo.getInterestReceived())-TaxYear2023.INTEREST_EXEMPTION;
+        }
+    }
+
+    calculateTaxableCapitalGain(){
+        if(Number(this.#taxInfo.getTotalCapitalGain())<=TaxYear2023.CAPITAL_EXEMPTION){
+            this.#taxableCapitalGain=0;
+        }else{
+            this.#taxableCapitalGain=(Number(this.#taxInfo.getTotalCapitalGain())-TaxYear2023.CAPITAL_EXEMPTION)*TaxYear2023.CAPITAL_EXEMPTION_RATE;
+        }
     }
 
 
