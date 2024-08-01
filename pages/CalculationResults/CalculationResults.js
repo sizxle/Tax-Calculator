7d7ead581cc216258b6a804a15166d2e8565664f
sthembizo.molefi <sthembizo@psybergate.co.za>
2024-08-01 17:09:15 +0200

    make it that we calculate there

--- pages/CalculationResults/CalculationResults.js
+++ pages/CalculationResults/CalculationResults.js
@@ -1,8 +1,6 @@
 window.onload = (event) => {
     const customer= Customer.fromJSON(JSON.parse(localStorage.getItem("customer")));
-    // const taxInfo=customer.getTaxInfo(0); //fix this
-    // const taxCalculator= new TaxCalculator(taxInfo);
-    // taxCalculator.compute();
+
 
 
   };
@@ -26,6 +24,45 @@ window.onload = (event) => {
     taxInfo.setRetirementFunds(formData.get("retirementFunding"));
     taxInfo.setTravelAllowance(formData.get("travelAllowance"));
     taxInfo.setDependents(formData.get("medicalDependencies"))
-    customer.addTaxInfo(taxInfo);
-    localStorage.setItem("customer",JSON.stringify(customer.toObject()))
+    // customer.addTaxInfo(taxInfo);
+    const taxCalculator= new TaxCalculator(taxInfo);
+    taxCalculator.compute();
+
+    console.log(taxCalculator.totalExpense)
+
+    showResults(taxCalculator);
+    // localStorage.setItem("customer",JSON.stringify(customer.toObject()))
   });
+
+  
+function showResults(taxCalculator){
+
+  const summary=document.getElementById("summary");
+  summary.style.display="block"
+
+  const totalIncomeTaxable= document.getElementById("total-income");
+  const income = taxCalculator.totalTaxableIncome;
+  const formattedIncome = new Intl.NumberFormat().format(income);
+  totalIncomeTaxable.innerText= `${formattedIncome}`;  
+  
+  const totalExpense= document.getElementById("total-expense-deductible");
+  totalExpense.innerText=taxCalculator.totalExpense;
+ 
+  const taxableIncome= document.getElementById("taxable-income");
+  taxableIncome.innerText=taxCalculator.amountToBeTaxed;
+
+  const taxPayable= document.getElementById("tax-payable");
+  taxPayable.innerText=taxCalculator.taxPayable;
+
+  const medicalCredit= document.getElementById("medical-credit");
+  medicalCredit.innerText=taxCalculator.medicalCredit;
+
+  const rebate= document.getElementById("rebate");
+  rebate.innerText=taxCalculator.rebate;
+
+  const netPayable= document.getElementById("nett-payable");
+  netPayable.innerText=taxCalculator.netTaxPayable;
+
+  const finalPayable= document.getElementById("final-payable");
+  finalPayable.innerText=taxCalculator.finalTaxPayable;
+}
